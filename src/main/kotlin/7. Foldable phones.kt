ce087508ee213ebcf0d93b674048ba69cfaede52
swapnil-up <swapnilupadhyaykharel@gmail.com>
2023-08-11 19:51:15 +0545

    solved Foldable phones problem. needed to write the main function too so I don't know if I really solved all the test cases. Maybe there was a way to declare a variable as phone then inherit it as flip phone too? Something to check in the future. writing         val folded=if(isFolded) "folded" else "not folded" was fun. the isFolded checks if true and one or the other string is entered into folded easily.
    class a:class b is how you inherit b's stuff to a
    classes are final by default so put open.
    function are also final so when you override in child make it open in parent

---
+++ src/main/kotlin/7. Foldable phones.kt
@@ -0,0 +1,42 @@
+open class Phone(var isScreenLightOn: Boolean = false){
+    open fun switchOn() {
+        isScreenLightOn = true
+    }
+
+    fun switchOff() {
+        isScreenLightOn = false
+    }
+
+    fun checkPhoneScreenLight() {
+        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
+        println("The phone screen's light is $phoneScreenLight.")
+    }
+}
+
+class FoldablePhone(isScreenLightOn: Boolean,var isFolded:Boolean=false):Phone(isScreenLightOn){
+    override fun switchOn(){
+        if(isFolded) println("unfold first")
+        else{
+            isScreenLightOn=true
+            println("welcome")
+        }
+    }
+    fun checkFolded(){
+        val folded=if(isFolded) "folded" else "not folded"
+        println("The phone's screen is $folded")
+    }
+    fun fold(){
+        isFolded=true
+    }
+    fun unfold(){
+        isFolded=false
+    }
+
+}
+
+fun main(){
+    var p=Phone(true)
+    var f=FoldablePhone(true,true)
+    p.switchOn()
+    f.switchOn()
+}
